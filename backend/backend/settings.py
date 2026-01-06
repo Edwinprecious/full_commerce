0da7671f6989c7a3404ad311b824f16cea0a4c94
Edwinprecious <100590844+Edwinprecious@users.noreply.github.com>
2026-01-06 13:05:27 +0100

    Trigger redeploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECRET_KEY = os.getenv("SECRET_KEY")
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="unsafe-dev-key")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 if not SECRET_KEY:

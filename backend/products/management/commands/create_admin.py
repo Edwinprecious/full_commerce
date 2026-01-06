1bfb8b0a9c0436d87ac0eaf81721c610d3ac7f4f
Edwinprecious <100590844+Edwinprecious@users.noreply.github.com>
2026-01-06 18:41:36 +0100

    Fix create_admin to use custom user model

--- backend/products/management/commands/create_admin.py
+++ backend/products/management/commands/create_admin.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 import os
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        User = get_user_model()  # get the custom user model
         if not User.objects.filter(username="admin").exists():
             User.objects.create_superuser(
                 username="admin",
